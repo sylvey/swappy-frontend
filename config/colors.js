7bf4e758a85022ed83b2c263db98a177f56f85d2
sylvey <80093140+sylvey@users.noreply.github.com>
2021-09-18 19:13:39 +0800

    Add files via upload

---
+++ config/colors.js
@@ -0,0 +1,24 @@
+/**
+ * @providesModule Colors
+ */
+// @providesModule can allow import anywhere
+ const colors = {
+    function_100: '#629D89',
+    function_80: '#629D89',
+    function_60: '#D2EFE5',
+    main_80:'#84C6B0',
+    main_100:'#629D89',
+    main_60: '#D2EFE5',
+    warning_100: '#EC9292',
+    warning_80: '#FFB0AE',
+    warning_60: '#FADAD9',
+    mono_100: '#404040',
+    mono_80: '#8D8D8D',
+    mono_60: '#D5D5D5',
+    mono_40: '#EDEDED',
+    brown_100: '#665D54',
+    brown_80: '#C9B19A',
+    brown_60: '#DDC9B6',
+    brown_40: '#EEE0D3'
+  };
+ export default colors;
